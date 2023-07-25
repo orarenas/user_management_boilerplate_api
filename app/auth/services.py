44a4d944d6231621a4c87edd101e2df7e3d3ac80
dot-oliver <orarenas@tourism.gov.ph>
2023-07-25 15:26:16 +0800

    Temporarily removed scope on /users/me for testing purposes

--- app/auth/services.py
+++ app/auth/services.py
@@ -106,7 +106,7 @@ async def get_current_user(security_scopes: SecurityScopes, token: Annotated[str
             )
     return user
 
-async def get_current_active_user(current_user: Annotated[User, Security(get_current_user, scopes=["me"])]):
+async def get_current_active_user(current_user: Annotated[User, Depends(get_current_user)]):
     """ if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user") """
     return current_user
